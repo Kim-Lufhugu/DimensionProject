e6a599931735f40fad7b053367572ace36dbf58f
William Phokompe <phokompe.william@outlook.com>
2020-11-21 21:39:53 +0200

    Edit entity

--- Entities/Employee.cs
+++ Entities/Employee.cs
@@ -7,11 +7,11 @@ namespace organisation_x_api.Entities
 {
     public class Employee
     {
-        public int id { get; set; }
+        public int Id { get; set; }
 
-        public string firstname { get; set; }
+        public string Firstname { get; set; }
 
-        public string lastname { get; set; }
+        public string Lastname { get; set; }
 
         public DateTime? DateEmployed { get; set; }
     }
